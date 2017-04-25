fd22842371b3716c2437ebe6b0ee90a6f5627574
hhami <huseyin.hamilton@travisperkins.co.uk>
2017-04-25 21:53:52 +0100

    some refactoring

--- code.py
+++ code.py
@@ -1,32 +1,40 @@
 class aocLevelOne:
+
     def calculateblocks(self, str):
         coordinates = [0,0]
-        pos = 0
+        position = 0
         splitinput = str.split(", ")
         for instruct in splitinput:
             direction = instruct[:1]
             movement = int(instruct[1:])
-            if(direction == 'R'):
-                if(pos == 270):
-                    pos = 0
-                else:
-                    pos += 90
-            if(direction == 'L'):
-                if(pos == 0):
-                    pos = 270
-                else:
-                    pos -= 90
-            if(pos == 0):
-                curr = coordinates[1] + movement
-                coordinates[1] = curr
-            if(pos == 90):
-                curr = coordinates[0] + movement
-                coordinates[0] = curr
-            if(pos == 180):
-                curr = coordinates[1] - movement
-                coordinates[1] = curr
-            if(pos == 270):
-                curr = coordinates[0] - movement
-                coordinates[0] = curr
-
+            position = self.adjustposition(direction, position)
+            coordinates = self.adjustcoordinates(movement, position, coordinates)
         return abs(coordinates[0]) + abs(coordinates[1])
+
+    def adjustposition(self, direction, position):
+        if(direction == 'R'):
+            if(position == 270):
+                position = 0
+            else:
+                position += 90
+        if(direction == 'L'):
+            if(position == 0):
+                position = 270
+            else:
+                position -= 90
+        return position
+
+    def adjustcoordinates(self, movement, position, coordinates):
+        if(position == 0):
+            curr = coordinates[1] + movement
+            coordinates[1] = curr
+        if(position == 90):
+            curr = coordinates[0] + movement
+            coordinates[0] = curr
+        if(position == 180):
+            curr = coordinates[1] - movement
+            coordinates[1] = curr
+        if(position == 270):
+            curr = coordinates[0] - movement
+            coordinates[0] = curr
+        return coordinates
