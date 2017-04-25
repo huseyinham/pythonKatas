c6afed0a71e95b7ec8249021337b4d8c6d15dc5f
huseyinham <hlhamilton@hotmail.co.uk>
2017-04-25 17:14:41 +0100

    first commit

---
+++ code.py
@@ -0,0 +1,37 @@
+class aocLevelOne:
+    def calculateblocks(self, str):
+        coordinates = [0,0]
+        pos = 0
+        splitinput = str.split(", ")
+        print splitinput
+        for instruct in splitinput:
+            direction = instruct[:1]
+            movement = int(instruct[1:])
+            if(direction == 'R'):
+                if(pos == 270):
+                    pos = 0
+                else:
+                    pos += 90
+            if(direction == 'L'):
+                if(pos == 0):
+                    pos = 270
+                else:
+                    pos -= 90
+            if(pos == 0):
+                curr = coordinates[1] + movement
+                coordinates[1] = curr
+                print curr
+            if(pos == 90):
+                curr = coordinates[0] + movement
+                coordinates[0] = curr
+                print curr
+            if(pos == 180):
+                curr = coordinates[1] - movement
+                coordinates[1] = curr
+            if(pos == 270):
+                curr = coordinates[0] - movement
+                coordinates[0] = curr
+
+        print abs(coordinates[0])
+        print abs(coordinates[1])
+        return abs(coordinates[0]) + abs(coordinates[1])
