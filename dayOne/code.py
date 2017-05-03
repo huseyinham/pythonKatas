456997c80b94bd435afda4f301a0d6d6545a98be
huseyinham <hlhamilton@hotmail.co.uk>
2017-05-03 10:25:49 +0100

    pinpoint duplicate coordinates and return them

--- dayOne/code.py
+++ dayOne/code.py
@@ -4,37 +4,41 @@ class aocLevelOne:
         coordinates = [0,0]
         position = 0
         splitinput = str.split(", ")
+        match = False
+        coordinatespassed = []
         for instruct in splitinput:
             direction = instruct[:1]
             movement = int(instruct[1:])
-            position = self.adjustposition(direction, position)
-            coordinates = self.adjustcoordinates(movement, position, coordinates)
+            if(direction == 'R'):
+                if(position == 270):
+                    position = 0
+                else:
+                    position += 90
+            if(direction == 'L'):
+                if(position == 0):
+                    position = 270
+                else:
+                    position -= 90
+
+            for move in xrange(movement):
+                if(position == 0):
+                    curr = coordinates[1] + 1
+                    coordinates[1] = curr
+                if(position == 90):
+                    curr = coordinates[0] + 1
+                    coordinates[0] = curr
+                if(position == 180):
+                    curr = coordinates[1] - 1
+                    coordinates[1] = curr
+                if(position == 270):
+                    curr = coordinates[0] - 1
+                    coordinates[0] = curr
+                if coordinates in coordinatespassed:
+                    match = True
+                    break
+                else:
+                    tempcoordinates = [coordinates[0],coordinates[1]]
+                    coordinatespassed.append(tempcoordinates)
+            if(match == True):
+                break
         return abs(coordinates[0]) + abs(coordinates[1])
-
-    def adjustposition(self, direction, position):
-        if(direction == 'R'):
-            if(position == 270):
-                position = 0
-            else:
-                position += 90
-        if(direction == 'L'):
-            if(position == 0):
-                position = 270
-            else:
-                position -= 90
-        return position
-
-    def adjustcoordinates(self, movement, position, coordinates):
-        if(position == 0):
-            curr = coordinates[1] + movement
-            coordinates[1] = curr
-        if(position == 90):
-            curr = coordinates[0] + movement
-            coordinates[0] = curr
-        if(position == 180):
-            curr = coordinates[1] - movement
-            coordinates[1] = curr
-        if(position == 270):
-            curr = coordinates[0] - movement
-            coordinates[0] = curr
-        return coordinates
--- dayOne/test.py
+++ dayOne/test.py
@@ -4,26 +4,53 @@ from code import aocLevelOne
 
 class aocLevelOneSpec(unittest.TestCase):
 
-    def fullTest(self):
+    global RIGHT
+    global LEFT
+    global NORTH
+    global EAST
+    global SOUTH
+    global WEST
+    global MOVEMENT
+    RIGHT = "R"
+    LEFT = "L"
+    NORTH = 0
+    EAST = 90
+    SOUTH = 180
+    WEST = 270
+    MOVEMENT = 1
+
+    def test_fullExample(self):
         self.assertEqual(aocLevelOne().calculateblocks
         ("R3, L5, R2, L1, L2, R5, L2, R2, L2, L2, L1, R2, L2, R4, R4, R1, L2, L3, R3, L1, R2, L2, L4, R4, R5, L3, R3, L3, L3, R4, R5, " +
         "L3, R3, L5, L1, L2, R2, L1, R3, R1, L1, R187, L1, R2, R47, L5, L1, L2, R4, R3, L3, R3, R4, R1, R3, L1, L4, L1, R2, L1, R4, R5, " +
         "L1, R77, L5, L4, R3, L2, R4, R5, R5, L2, L2, R2, R5, L2, R194, R5, L2, R4, L5, L4, L2, R5, L3, L2, L5, R5, R2, L3, R3, R1, L4, R2, " +
         "L1, R5, L1, R5, L1, L1, R3, L1, R5, R2, R5, R5, L4, L5, L5, L5, R3, L2, L5, L4, R3, R1, R1, R4, L2, L4, R5, R5, R4, L2, L2, R5, " +
         "R5, L5, L2, R4, R4, L4, R1, L3, R1, L1, L1, L1, L4, R5, R4, L4, L4, R5, R3, L2, L2, R3, R1, R4, L3, R1, L4, R3, L3, L2, R2, R2, " +
-        "R2, L1, L4, R3, R2, R2, L3, R2, L3, L2, R4, L2, R3, L4, R5, R4, R1, R5, R3"), 243)
+        "R2, L1, L4, R3, R2, R2, L3, R2, L3, L2, R4, L2, R3, L4, R5, R4, R1, R5, R3"), 142)
 
-    def rightTwoLeftOneLeftTwoReturnsOneBlock(self):
+    def test_rightTwoLeftOneLeftTwoReturnsOneBlock(self):
         self.assertEqual(aocLevelOne().calculateblocks("R2, L1, L2"), 1)
 
-    def leftTwoRightOneRightTwoReturnsOneBlock(self):
+    def test_leftTwoRightOneRightTwoReturnsOneBlock(self):
         self.assertEqual(aocLevelOne().calculateblocks("L2, R1, R2"), 1)
 
-    def leftOneLeftOneReturnsTwoBlocks(self):
+    def test_leftOneLeftOneReturnsTwoBlocks(self):
         self.assertEqual(aocLevelOne().calculateblocks("L1, L1"), 2)
 
-    def rightOneRightOneReturnsTwoBlocks(self):
+    def test_rightOneRightOneReturnsTwoBlocks(self):
         self.assertEqual(aocLevelOne().calculateblocks("R1, R1"), 2)
+
+    def test_rightOneRightOneRightOneRightOneReturnsZeroBlocks(self):
+        self.assertEqual(aocLevelOne().calculateblocks("R1, R1, R1, R1"), 0)
+
+    def test_leftOneLeftOneLeftOneLeftOneReturnsZeroBlocks(self):
+        self.assertEqual(aocLevelOne().calculateblocks("L1, L1, L1, L1"), 0)
+
+    def test_rightTwoRightOneRightOneRightOneReturnsZeroBlocks(self):
+        self.assertEqual(aocLevelOne().calculateblocks("R2, R1, R1, R1"), 1)
+
+    def test_leftTwoLeftOneLeftOneLeftOneReturnsZeroBlocks(self):
+        self.assertEqual(aocLevelOne().calculateblocks("L2, L1, L1, L1"), 1)
 
 if __name__ == '__main__':
     unittest.main()
